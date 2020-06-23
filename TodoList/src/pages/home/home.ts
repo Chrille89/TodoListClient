d31a9473eeedec9d8281bca6858c59b3b0b43ac6
Christian Bach <christian.bach@th-wildau.de>
2020-06-23 17:57:52 +0200

    post,get,delete

--- TodoList/src/pages/home/home.ts
+++ TodoList/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -9,17 +11,44 @@ export class HomePage {
 
   private taskList : any[] = [];
 
+  constructor(private httpClient : HttpClient, public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ngOnInit() {
+   this.getTasks();
   }
 
   public addTask() : void {
     if(this.taskName && this.taskName.length > 0) {
-      this.taskList.push(this.taskName);
+      let task = {};
+      task.label =  this.taskName;
       this.taskName = "";
+      this.postTask(task);
     }
- 
   }
 
+  public completeTask(id : number) : void {
+    this.deleteTask(id);
+  }
+
+  public getTasks() : void {
+    this.httpClient.get("http://localhost:8080/all").subscribe((data: any[]) => {
+      this.taskList = data;
+    });
+  }
+
+  public postTask(task : any){
+    this.httpClient.post("http://localhost:8080/new",task).subscribe((data: any) => {
+      console.log("Add new task: ",data);
+      this.getTasks();
+    });
+  }
+
+  public deleteTask(id:number) : void {
+    this.httpClient.delete("http://localhost:8080/"+id).subscribe((data: any[]) => {
+      console.log("Delete task with id: ",id);
+      this.getTasks();
+    });
+  }
 }
